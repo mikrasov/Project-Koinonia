f0f76982fa2d12d81107f09da6a701fdabd96aaa
Michael Nekrasov <mikrasov@cs.ucsb.edu>
2015-05-16 12:46:00 -0700

    Heroku OS

--- koinonia/settings.py
+++ koinonia/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
